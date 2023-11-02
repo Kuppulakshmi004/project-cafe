79116812adb330584d820e6fadc3b5578933c2e1
kupps004 <kuppulakshmi61@gmail.com>
2023-11-02 16:37:17 +0530

    first commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import img1 from "../img/pic1.jpg";
+import logo from "../img/27cce497-1a6f-42ef-ae7d-c584c1244f1f.jpg";
 
 
 function Home() {
   return (
+    
     <div className="home">
       <img className="img1" src={img1}></img>
       </div>
